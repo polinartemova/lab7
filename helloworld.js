4c15fd659940246b76840ba0c3a816793eb0e70c
polinartemova <39812708+polinartemova@users.noreply.github.com>
2019-05-05 17:49:43 +0300

    Add files via upload

---
+++ helloworld.js
@@ -0,0 +1,75 @@
+var groupmates = [
+{
+"name": "Олеся",
+"group": "1702",
+"age": 19,
+"marks": [5, 5, 5, 5, 5]
+},
+{
+"name": "Катя",
+"group": "1702",
+"age": 20,
+"marks": [5, 4, 5, 5, 5]
+},
+{
+"name": "Алина",
+"group": "1703",
+"age": 19,
+"marks": [3, 5, 4, 3, 5]
+},
+{
+"name": "Роман",
+"group": "1703",
+"age": 18,
+"marks": [5, 4, 5, 4, 4]
+},
+{
+"name": "Настя",
+"group": "1703",
+"age": 19,
+"marks": [2, 3, 3, 3, 4]
+}
+];
+console.log(groupmates);
+
+var rpad = function(str, length) {
+str = str.toString(); // преобразование в строку
+while (str.length < length)
+str = str + ' '; // добавление пробела в конец строки
+return str; // когда все пробелы добавлены, возвратить строку
+};
+var printStudents = function(students){
+console.log(
+rpad("Имя студента", 15),
+rpad("Группа", 8),
+rpad("Возраст", 8),
+rpad("Оценки", 20)
+);
+// был выведен заголовок таблицы
+for (var i = 0; i<=students.length-1; i++){
+// в цикле выводится каждый экземпляр студента
+console.log(
+rpad(students[i]['name'], 15),
+rpad(students[i]['group'], 8),
+rpad(students[i]['age'], 8),
+rpad(students[i]['marks'], 20)
+);
+}
+console.log('\n'); // добавляется пустая строка в конце вывода
+};
+printStudents(groupmates);
+
+var filterStudents = function (students, groupNumber) { 
+	for (var i in students) { 
+		if(students[i]['group']==groupNumber){ 
+			console.log( 
+			rpad(students[i]['name'], 15), 
+			rpad(students[i]['group'], 8), 
+			rpad(students[i]['age'], 8), 
+			rpad(students[i]['marks'], 20) 
+			); 
+		} 
+	} 
+} 
+
+filterStudents(groupmates, "1702");
